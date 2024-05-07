037048d3b3b87c38173cddb5dd679bc11cde9b09
Elliot Wolk <elliot.wolk@gmail.com>
2024-05-07 12:21:13 -0400

    smutils[files]: move SONG_PARENTS_DIR ddr-stepmania-score => smutils

--- src/SMUtils/Files.pm
+++ src/SMUtils/Files.pm
@@ -7,6 +7,7 @@ our @EXPORT_OK = qw();
 our @EXPORT = qw(
 );
 
+our $DIR_SONGS_PARENT = "$ENV{HOME}/.stepmania-5.0";
 
 our $DIR_XML_CACHE_BASE = "$ENV{HOME}/.cache/stepmania-score-xml";
 our $DIR_XML_CACHE_SCORES = "$DIR_XML_CACHE_BASE/scores";
